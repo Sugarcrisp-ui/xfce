65f5a06c2528ee7bd475dbe286bc14a905bd05cd
Sugarcrisp-ui <66784587+Sugarcrisp-ui@users.noreply.github.com>
2020-06-18 15:26:49 +0700

    Update 110-install-core-software-v3.sh

--- 110-install-core-software-v3.sh
+++ 110-install-core-software-v3.sh
@@ -42,6 +42,7 @@ sudo pacman -S --noconfirm --needed gimp
 sudo pacman -S --noconfirm --needed variety
 sudo pacman -S --noconfirm --needed bitwarden
 sudo pacman -S --noconfirm --needed chromium
+sudo pacman -S --noconfirm --needed firefox
 sudo pacman -S --noconfirm --needed arcolinuxtweak-tool-git
 sudo pacman -S --noconfirm --needed xfce4-whiskermenu-plugin
 sudo pacman -S --noconfirm --needed pamac
