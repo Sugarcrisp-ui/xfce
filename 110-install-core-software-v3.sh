fe3e1448ce7d23c4e61b539165227b8dd0d2c119
Sugarcrisp-ui <66784587+Sugarcrisp-ui@users.noreply.github.com>
2020-06-18 09:19:01 +0700

    Add files via upload

---
+++ 110-install-core-software-v3.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author 	: Erik Dubois
+# Website : https://www.erikdubois.be
+# Website	: https://www.arcolinux.info
+# Website	:	./https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+#software from Arch Linux repositories
+
+sudo pacman -S --noconfirm --needed xfce4-whiskermenu-plugin
+sudo pacman -S --noconfirm --needed celluloid
+sudo pacman -S --noconfirm --needed clipgrab
+sudo pacman -S --noconfirm --needed gparted
+sudo pacman -S --noconfirm --needed dconf
+sudo pacman -S --noconfirm --needed spotify
+sudo pacman -S --noconfirm --needed geany
+sudo pacman -S --noconfirm --needed insync
+sudo pacman -S --noconfirm --needed bitwarden-bin
+sudo pacman -S --noconfirm --needed catfish
+sudo pacman -S --noconfirm --needed grub-customizer
+sudo pacman -S --noconfirm --needed virtualbox
+sudo pacman -S --noconfirm --needed virtualbox-guest-utils
+sudo pacman -S --noconfirm --needed virtualbox-host-dkms
+sudo pacman -S --noconfirm --needed qbittorrent
+sudo pacman -S --noconfirm --needed redshift
+sudo pacman -S --noconfirm --needed redshift-gtk
+sudo pacman -S --noconfirm --needed gnome-screenshot
+sudo pacman -S --noconfirm --needed viber
+sudo pacman -S --noconfirm --needed libreoffice-fresh
+sudo pacman -S --noconfirm --needed meld
+sudo pacman -S --noconfirm --needed udisks2
+sudo pacman -S --noconfirm --needed grsync
+sudo pacman -S --noconfirm --needed gimp
+sudo pacman -S --noconfirm --needed variety
+sudo pacman -S --noconfirm --needed bitwarden
+sudo pacman -S --noconfirm --needed chromium
+sudo pacman -S --noconfirm --needed arcolinuxtweak-tool-git
+sudo pacman -S --noconfirm --needed xfce4-whiskermenu-plugin
+sudo pacman -S --noconfirm --needed pamac
+sudo pacman -S --noconfirm --needed conky-lua-archers
+
+#sudo pacman -S --noconfirm --needed
+#sudo pacman -S --noconfirm --needed arcolinux-meta-fun
+
+
+
+
+
+
+###############################################################################################
+
+echo "################################################################"
+echo "################### core software installed"
+echo "################################################################"
