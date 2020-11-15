ca5523fafa7bd33d3aad43b12f349611851771b0
Brett <brettcrisp2@gmail.com>
2020-11-15 14:05:59 +0700

    Update minimal-start-here-brett-v1.sh

--- minimal-start-here-brett-v1.sh
+++ minimal-start-here-brett-v1.sh
@@ -32,9 +32,6 @@ cd ~/DATA/arco-xfce-brett
 sh 100-display-manager-and-xfce-desktop*
 sh 110-install-core-software*
 sh 200-software-AUR-repo*
-#sh 300-sardi-extra-icons-AUR-repo*
-#sh 310-sardi-mint-y-icons-AUR-repo*
-#sh 320-surfn-mint-y-icons-git-AUR-repo*
 
 cd ~/DATA/arco-xfce-brett/AUR
 
@@ -53,14 +50,10 @@ sh 666-uninstall-applications*
 sh 667-uninstall-arcolinux-meta-steam*
 sh 900-install-personal-settings-folders*
 sh 915-install-personal-settings-screenshot-to-jpg*
-#sh 920-fix-simplescreencorder-icons*
-#sh 925-fix-sublime-text-icons*
 sh 930-autostart-applications*
-#sh 935-plasma-specific-applications-and-setttings*
 sh 950-install-all-fonts-needed-for-conkys-of-arcolinux*
 sh 955-install-settings-autoconnect-to-bluetooth-headset-v*
-#sh 960-ckb-next-v*
-#sh 965-install-key-servers-v*
+
 tput setaf 1
 echo "################################################################"
 echo "DO NOT INSTALL THIS"
