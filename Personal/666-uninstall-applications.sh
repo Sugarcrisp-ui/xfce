51797439705b436d411026db04eec5e1d8b34329
Brett <brettcrisp2@gmail.com>
2021-06-06 10:25:48 +0700

    Update 666-uninstall-applications.sh

--- Personal/666-uninstall-applications.sh
+++ Personal/666-uninstall-applications.sh
@@ -1,44 +1,64 @@
 #!/bin/bash
+
 # The set command is used to determine action if error 
 # is encountered.  (-e) will stop and exit (+e) will 
 # continue with the script.
 set +e
 ##################################################################################################################
 
-sudo pacman -R --noconfirm evolution
-sudo pacman -R --noconfirm orage
-sudo pacman -R --noconfirm malcontent
-sudo pacman -R --noconfirm guvcview
-sudo pacman -R --noconfirm inkscape
-sudo pacman -R --noconfirm pragha
-sudo pacman -R --noconfirm simplescreenrecorder
-sudo pacman -R --noconfirm midori
-sudo pacman -R --noconfirm vlc
-#sudo pacman -R --noconfirm termite
-sudo pacman -R --noconfirm synapse
-sudo pacman -R --noconfirm gpick
-sudo pacman -R --noconfirm plank
-sudo pacman -R --noconfirm nomacs
-#sudo pacman -R --noconfirm youtube-dl
-sudo pacman -R --noconfirm screenkey
-sudo pacman -R --noconfirm temps
-sudo pacman -R --noconfirm wttr
-sudo pacman -R --noconfirm xfburn
-sudo pacman -R --noconfirm caja
-sudo pacman -R --noconfirm pulseaudio-equalizer-ladspa
-sudo pacman -R --noconfirm vim
-sudo pacman -R --noconfirm mousepad
-sudo pacman -R --noconfirm totem
-#sudo pacman -R --noconfirm udisks2
-sudo pacman -R --noconfirm arcolinux-meta-fun
-sudo pacman -R --noconfirm vivaldi-ffmpeg-codecs
-sudo pacman -R --noconfirm vivaldi-widevine
-sudo pacman -R --noconfirm vivaldi
-sudo pacman -R --noconfirm brave
-sudo pacman -R --noconfirm parole
-sudo pacman -R --noconfirm xfce4-evince
-#sudo pacman -R --noconfirm nemo
+function_remove() {
+	if pacman -Qi $1 &> /dev/null; then
+		tput setaf 1
+		echo "################################################################"
+		echo "######    "$1" is installed and will be removed now."
+		echo "################################################################"
+		echo 
+		tput sgr0
+		sudo pacman -Rs $1 --noconfirm
+	else
+		tput setaf 2
+		echo "################################################################"
+		echo "######    "$1" was not present or already removed."
+		echo "################################################################"
+		echo 
+		tput sgr0
+	fi
+}
+
+
+function_remove evolution
+function_remove orage
+function_remove malcontent
+function_remove guvcview
+function_remove inkscape
+function_remove pragha
+function_remove simplescreenrecorder
+function_remove midori
+function_remove vlc
+#function_remove termite
+function_remove synapse
+function_remove gpick
+function_remove plank
+function_remove nomacs
+function_remove screenkey
+function_remove temps
+function_remove wttr
+function_remove xfburn
+function_remove caja
+function_remove pulseaudio-equalizer-ladspa
+function_remove vim
+function_remove mousepad
+function_remove totem
+#function_remove udisks2
+function_remove arcolinux-meta-fun
+function_remove vivaldi-ffmpeg-codecs
+function_remove vivaldi-widevine
+function_remove vivaldi
+function_remove brave
+function_remove parole
+function_remove xfce4-evince
+function_remove nemo
 
 echo "################################################################"
-echo "####                      packages uninstalled            ######"
+echo "######                    packages uninstalled            ######"
 echo "################################################################"
